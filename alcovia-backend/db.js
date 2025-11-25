212ad7429c7dd00e60d42b5b2c16fe13a1eed6a3
baradisiddarthareddy <sidharthreddy9966@gmail.com>
2025-11-26 00:05:01 +0530

    Updated server.js and db.js file

--- alcovia-backend/db.js
+++ alcovia-backend/db.js
@@ -1,12 +1,16 @@
-import mysql from "mysql2";
+const mysql = require("mysql2");
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  })
+  .promise();
+
+module.exports = db;
